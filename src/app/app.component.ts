aa637cd8b736ca4474b2e38bb19727a1137c663d
Md Hilwan <wan.wanted.1@gmail.com>
2021-08-31 17:54:16 -0500

    start add dummy cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'resume-editor';
+  basicInfo: any = {};
+
+  ngOnInit(): void {
+    this.basicInfo.name = faker.name.lastName();
+    this.basicInfo.nameHighlight = faker.name.firstName();
+    this.basicInfo.currentPosition = faker.name.jobTitle();
+    this.basicInfo.contact = faker.internet.email()
+  }
+
 }
